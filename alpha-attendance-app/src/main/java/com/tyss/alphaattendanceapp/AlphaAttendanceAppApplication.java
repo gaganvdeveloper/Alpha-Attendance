ab24cd156c19706b1ffdc0a01914655f4e610b86
gaganvdeveloper <gaganvdeveloper@gmail.com>
2023-10-18 22:42:55 +0530

    Project Created Successffully...

---
+++ alpha-attendance-app/src/main/java/com/tyss/alphaattendanceapp/AlphaAttendanceAppApplication.java
@@ -0,0 +1,13 @@
+package com.tyss.alphaattendanceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaAttendanceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaAttendanceAppApplication.class, args);
+	}
+
+}
